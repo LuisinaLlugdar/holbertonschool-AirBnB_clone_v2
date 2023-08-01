2558d83b407303c2c114806211e9f3994a1e14ca
Joy-Amorin <amorinkaren2021@gmail.com>
2023-08-01 13:27:53 -0700

    declarative_base

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class BaseModel:
-    """ base class for all hbnb models"""
+    """A base class for all hbnb models"""
 
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
